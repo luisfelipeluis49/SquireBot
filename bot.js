19a9cff8109a3ac32bce5a620dd59be3fe65b3c8
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2021-01-28 08:27:06 -0300

    update 0.5

--- bot.js
+++ bot.js
@@ -1,72 +1,159 @@
+//Discord.js
 const Discord = require("discord.js");
 const client = new Discord.Client();
+//Configs
 const config = require("./config.json");
+//Database
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('characterSheets.json')
+const db = low(adapter)
 
+//Initialize
 client.on("ready", () => {
-    console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`);
+    console.log(` Olá Mundo (de novo)\n Hoje o céu está tão cinza e melancólico...\n Perfeito para acender um cigarro e tomar um café a beira da piscina.`);
     client.user.setPresence({game: {name: 'comando', type: 1, url: 'https://www.twitch.tv/pedroricardo'} });
 });
+client.login(config.token);
 
+//Comands
 client.on("message", async message => {
 
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
     if(!message.content.startsWith(config.prefix)) return;
+    if(!message.guild) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const comando = args.shift().toLowerCase();
   
+  //Connection test
   if(comando === "ping") {
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
+    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
+  }
+
+  if(comando === "databook") {
+    db.set(message.guild.id, []).write()
+    message.channel.send('The Librarian started the confection, when you need to request or change an information on databook, you\'ll just need to ask.')
+  }
+
+  //Tabletop Audio
+  if(comando === "playt") {
+    if(!args[0]) return message.reply('put a tabletop link after the play.');
+    let [playableLink] = args;
+    let tabletopVerify = playableLink.split('.');
+    if (tabletopVerify[0] != 'http://tabletopaudio') return message.reply('We only accept links from [https://tabletopaudio.com]'); 
+    if (!message.member.voice.channel) return message.reply('You need to join a voice channel first!');
+    const connection = await message.member.voice.channel.join();
+
+    const dispatcher = connection.play(playableLink);
+    const m = await message.channel.send(`Playing [${playableLink}]`);
+    dispatcher.on('finish', () => {
+        m.edit(`The stream has finished`);
+      });
+  }
+
+  if(comando === "play") { 
+    if (!message.member.voice.channel) return message.reply('You need to join a voice channel first!');
+    const connection = await message.member.voice.channel.join();
+
+    const dispatcher = connection.play('./assets/music/biodome.mp3');
+    const m = await message.channel.send(`Playing [biodome]`);
+  }
+
+  //Sheets
+  if(comando === "sheet") {
+    if(!args[0]) {
+        return message.channel.send(
+            `If you don't wanna help me, then I would not help you.`, 
+            { 
+                files: [
+                    `./assets/archive/editableSheet.pdf`
+                ] 
+            });
+    }
+    let [characther] = args;
+    db.get(message.guild.id)
+    .push({
+      id: message.author.id,
+      name: characther,
+      class: 'barbarian',
+      level: 1,
+      race: 'stout halfling',
+      background: 'hero',
+      aligment: 'cn',
+      xp: 0, //Experience Point
+      strength: [20, 5, 7],
+      dexterity: [15, 2, 2],
+      constitution: [20, 5, 7],
+      intelligence: [6, -2, -2],
+      wisdom: [7, -2, -2],
+      charisma: [15, 2, 2],
+      hp: 17, //Hit Point
+      armour: 17,
+      hd: '1d12', //Hit Dice
+      initiative: 2,
+      speed: 25,
+      proeficience: 2,
+    }).write()
+    message.channel.send('Your sheet is filled with a Barbarian Halfling by default. You can edit the basics here so the DM can easylly see it, but I recomends you to use the editable sheet to a full experience.')
+
+  }
+
+  //DM Shields
+  /*
+   * if(comando === "combate") {
+   *   const m = await message.channel.send(`Eu estar aprendendo português ainda. Desculpa.`, { files: ['./assets/image/combat_ptBR.png'] }); 
+   * }
+   *
+   * if(comando === "explorar") {
+   *   const m = await message.channel.send(`Eu estar aprendendo português ainda. Desculpa.`, { files: ['./assets/image/exploration_ptBR.png'] }); 
+   * }
+   *
+   * if(comando === "social") {
+   *   const m = await message.channel.send(`Eu estar aprendendo português ainda. Desculpa.`, { files: ['./assets/image/social_ptBR.png'] }); 
+   * }
+   */
+
+  if(comando === "shield") {
+    if(!args[0]) {
+        return message.channel.send(
+        `I founded!`, 
+        { 
+            files: [
+                `./assets/image/dmShield1.png`, 
+                `./assets/image/dmShield2.png`, 
+                `./assets/image/dmShield3.png`
+            ] 
+        });
+    }
+    let [shieldNum] = args;
+    if(shieldNum != 1 && shieldNum != 2 && shieldNum != 3) {
+        return message.channel.send(`I am sorry master, but I think should try type [1, 2 or 3], instead of ${shieldNum}`);
+    }
+    
+    await message.channel.send("I would gladly be your shield if needed sir, but I think this would not help you right now.\nLet me search for a Dungeon Master Shield in my bag...");
+    await message.channel.send(`I founded!`, { files: [`./assets/image/dmShield${shieldNum}.png`] });
   }
 
   //Ramdomize dice rollings
+  if(comando === "roll") {
+    if(!args[0]) return message.channel.send('I am sorry master, but roll what?');
+    let [temp] = args;
+    let dice = temp.split('d');
+    if( (Number.isNaN(dice[0] * 1)) && (Number.isNaN(dice[1] * 1)) ) return message.channel.send('I am sorry master, but this kind of dice even exist?');
 
-  if(comando === "roll4") {
     const roll = await message.channel.send("Rolling...");
-    const num = Math.floor(Math.random() * (5 - 1)) + 1;
-    roll.edit(`The rolling was ${num}`);
-  }
-
-  if(comando === "roll6") {
-    const roll = await message.channel.send("Rolling...");
-    const num = Math.floor(Math.random() * (7 - 1)) + 1;
-    roll.edit(`The rolling was ${num}`);
-  }
-
-  if(comando === "roll8") {
-    const roll = await message.channel.send("Rolling...");
-    const num = Math.floor(Math.random() * (9 - 1)) + 1;
-    roll.edit(`The rolling was ${num}`);
-  }
-
-  if(comando === "roll10") {
-    const roll = await message.channel.send("Rolling...");
-    const num = Math.floor(Math.random() * (11 - 1)) + 1;
-    roll.edit(`The rolling was ${num}`);
-  }
-
-  if(comando === "roll12") {
-    const roll = await message.channel.send("Rolling...");
-    const num = Math.floor(Math.random() * (13 - 1)) + 1;
-    roll.edit(`The rolling was ${num}`);
-  }
-
-  if(comando === "roll20") {
-    const roll = await message.channel.send("Rolling...");
-    const num = Math.floor(Math.random() * (21 - 1)) + 1;
-    roll.edit(`The rolling was ${num}`);
-  }
-
-  if(comando === "roll100") {
-    const roll = await message.channel.send("Rolling...");
-    const num = Math.floor(Math.random() * (101 - 1)) + 1;
+    var rollNum = 0;
+    for (let diceNum = 0; diceNum < dice[0]; diceNum++) {
+        rollNum = rollNum + Math.floor(Math.random() * (dice[1])) + 1;
+    }
+    const num = rollNum;
     roll.edit(`The rolling was ${num}`);
   }
 
   //Ramdomize Coin Loots
-
   if(comando === "lootcoin") {
     const loot = await message.channel.send("looting...");
     const coin = Math.floor(Math.random() * (101 - 1)) + 1;
@@ -74,7 +161,6 @@ client.on("message", async message => {
   }
 
   //Ramdomize Loots
-
   if(comando === "loottiny") {
     for(var i = 0; i < 3; i++){
         var loot = await message.channel.send("looting...");
@@ -234,67 +320,29 @@ client.on("message", async message => {
   }
 
   if(comando === "somequest"){
-    const firstQuest = ['Adri','Eb','Del','Mar','Kris','Gun','Per','Ive','Had','Sar','Val','Lesh','Try','Gar','Wel','Jil','Pa','Os','Dum','Hel','Ivo','Ol','Cha','Yu','Ak','Leu','Mor','Ea','Ne','Phe'];
-    const secondQuest = ['be','erk','gran','da','ga','dred','an','mo','eli','lynn','si','nai','ton','bo','dal','ena','ela','tia','an','chen','ang','la','chro','non','ke','cis','bry','is','eta','ka'];
-    const thirdQuest = ['seis','ch','mon','sk','nt','ump','ddo','ryn','ock','thi','xius','jir','sfr','treg','hyu'];
-    const fourthQuest = ['sik','grim','hek','vol','liss','vol','in','id','men','ing','dal','dd','ra','rey','tra'];
-    const foundedQuest = Math.floor(Math.random() * (3 - 1)) + 1;
+    const questBank = ['Adri','Eb','Del','Mar','Kris','Gun','Per','Ive','Had','Sar','Val','Lesh','Try','Gar','Wel','Jil','Pa','Os','Dum','Hel','Ivo','Ol','Cha','Yu','Ak','Leu','Mor','Ea','Ne','Phe'];
+    const foundedQuest = Math.floor(Math.random() * (2)) + 1;
 
-    var questThinker = await message.channel.send("searching for some quest...");
+    let questThinker = await message.channel.send("searching for some quest...");
     wait(4000);
-    var howManyNeeds = Math.floor(Math.random() * (13 - 1)) + 1;
-    var quest;
-    if(foundedQuest === 2){
-        for(var i = 0; i < howManyNeeds; i++){
-            if(i === 0){
-                quest = firstQuest[Math.floor(Math.random() * (31 - 1))];
-            } else if(i === 3){
-                quest = quest + secondQuest[Math.floor(Math.random() * (31 - 1))];
-            } else if(i === 8){
-                quest = quest + thirdQuest[Math.floor(Math.random() * (16 - 1))];
-            } else if(i === 10){
-                quest = quest + fourthQuest[Math.floor(Math.random() * (16 - 1))];
-            }
-        }
+    let whatQuest = Math.floor(Math.random() * (100)) + 1;
+    let quest;
+    if(foundedQuest === 2) {
+        quest = questBank[whatQuest];
         questThinker.edit(`I founded this: ${quest}`);
-    } else{
+    }
+    else {
         questThinker.edit(`I didn't founded anything sir, maybe if i look around again...`);
     }
   }
   
 });
 
-client.login(config.token);
-
-/*async function lootItems() {
-    var loot = await message.channel.send("looting...");
-    var itemGroup = Math.floor(Math.random() * (101 - 1)) + 1;
-    var item;
-        if (itemGroup < 40) {
-            item = " bauble";
-        } 
-        else if (itemGroup < 60) {
-            item = "n under Level";
-        } 
-        else if (itemGroup < 80) {
-            item = " your level";
-        } 
-        else if (itemGroup < 95) {
-            item = "n over Level";
-        } 
-        else if (itemGroup < 99) {
-            item = "n epic";
-        }
-        else {
-            item = " legendary";
-        }
-    loot.edit(`The player[s] founded a${item} item`);
-}*/
-
-function wait(ms)
-{
-var d = new Date();
-var d2 = null;
-do { d2 = new Date(); }
-while(d2-d < ms);
+//Usefull Function
+function wait(ms) {
+    var d = new Date();
+    var d2 = null;
+    do {
+        d2 = new Date();
+    } while (d2-d < ms);
 }
